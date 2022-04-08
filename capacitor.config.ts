a53e9666787175e4be1aeceaec450b0b9cde4174
cedric-charlesia <ccharlesia@gmail.com>
2022-04-08 09:50:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mon-budget-ionic-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
