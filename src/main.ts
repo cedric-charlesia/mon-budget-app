7db37b64f304c18b18721d4f27b39d8ed9431465
cedric-charlesia <ccharlesia@gmail.com>
2022-04-12 21:44:09 +0200

    Ionic: setup for PWA

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// Service Worker for Progressive Web App
+import './registerServiceWorker';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(createPinia())
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://mon-budget-api.herokuapp.com/",
     withCredentials: false,
     headers: {
       Accept: "application/json",
