59d2123ebc67b40df41a6c428f8ca1499649262a
cedric-charlesia <ccharlesia@gmail.com>
2022-05-13 15:40:00 +0200

    Quasar: PWA setup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,19 @@
     <h2 class="text-h5 text-center q-mx-lg q-my-lg">L'application personnalis&eacute;e pour suivre ses d&eacute;penses
       avec simplicit&eacute; !</h2>
     <HomePageForms />
+
+
+    <q-card bordered square class="my-card q-my-xl q-mx-md absolute-bottom">
+      <q-banner inline-actions rounded class="bg-white text-primary q-gutter-md">
+        <q-icon size="md"><img src="../../mon-budget-logo.png" /></q-icon>
+
+        <template v-slot:action>
+          <q-btn flat label="Annuler" />
+          <q-btn color="primary" label="Installer" />
+        </template>
+      </q-banner>
+    </q-card>
+
   </q-page>
 </template>
 
