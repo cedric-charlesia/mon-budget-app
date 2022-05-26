3dd377fe53bb05279d2114a3fd4a62daba45cfe3
cedric-charlesia <ccharlesia@gmail.com>
2022-05-26 18:45:40 +0200

    Responsive design desktop update + categories container page bug fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,6 +4,17 @@
       personnalis&eacute;e et avec simplicit&eacute; !</h2>
     <HomePageForms />
 
+    <!-- <q-card bordered square class="my-card q-my-xl q-mx-md absolute-bottom" v-if="deferredPrompt">
+      <q-banner inline-actions rounded class="bg-white text-primary q-gutter-md">
+        <q-icon size="md"><img src="../../mon-budget-logo.png" /></q-icon>
+
+        <template v-slot:action>
+          <q-btn flat label="Annuler" @click="deferredPrompt = false" />
+          <q-btn color="primary" label="Installer" @onClick="promptInstall" />
+        </template>
+      </q-banner>
+    </q-card> -->
+
   </q-page>
 </template>
 
@@ -17,5 +28,24 @@ defineComponent({
   name: 'IndexPage',
   components: { HomePageForms },
 });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// let deferredPrompt: BeforeInstallPromptEvent | any = ref(true);
+
+// const promptInstall = () => {
+//   deferredPrompt.value.prompt();
+
+//   deferredPrompt.value.userChoice.then((choiceResult: { outcome: string; }) => {
+//     if (choiceResult.outcome === 'accepted') {
+//       console.log('User accepted the install prompt');
+//     } else {
+//       console.log('User dismissed the install prompt');
+//     }
+
+//     deferredPrompt = null;
+//     console.log(deferredPrompt);
+
+//   });
+// };
 
 </script>
